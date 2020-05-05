180ed662fba81fb205c6d7e6c3226a6ec92dd082
fabcataldo <fabioalbertocataldo@gmail.com>
2020-05-05 16:51:27 -0300

    Sigo con la implementación de la pantalla de login.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,80 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Form, Input, Button, Checkbox, Layout, Menu, Row } from 'antd';
 import 'antd/dist/antd.css';
 import '../styles/appStyles.scss';
 import background from '../assets/imgs/background.jpg'
 const { Header, Footer, Content } = Layout;
 
 function Login() {
+  const onFinish = values => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
   return (
-      <Layout className="layout">
-        <Header className="headerStyle">
-          <div className="logo" />
-        </Header>
-        <Content>
-            <div className="site-layout-content" styles={{ backgroundImage:`url(${background})` }}>
-                Usuario
-            </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-      </Layout>
+    <Layout className="layout">
+      <Header className="headerStyle">
+        <div className="logo" />
+      </Header>
+      <Content>
+        <div className="site-layout-content" style={{ backgroundImage: `url(${background})`, textAlign: "center"}}>
+          <h1 style={{color: "white"}}>
+            Bienvenido a CSaleAdminWeb! :)
+          </h1>
+          <Form
+            layout="vertical"
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          
+          >
+            <Row   style={{justifyContent: "center"}}>
+              <Form.Item
+                label="Usuario"
+                name="username"
+                className="labelStyle"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your username!',
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Row>
+            
+            <Row  style={{justifyContent: "center"}}> 
+            <Form.Item
+              label="Contraseña"
+              name="password"
+                
+              style={{color:"white"}}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+            </Row>
+            <Form.Item>
+              <Button className="button" type="primary" htmlType="submit">
+                Iniciar sesión
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}  className="footer">CSaleAdminWeb (c) 2020</Footer>
+    </Layout>
   );
 }
 
