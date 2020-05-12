cb817fd0674ac23761bff2ecc8b4001a7211ad38
fabcataldo <fabioalbertocataldo@gmail.com>
2020-05-12 19:30:06 -0300

    Validar ticket: mejoro estilos, incorporo el logo en el header de la pág. Implemento la muestra tanto del ticket validado como de su usuario. Mejoro la muestra de los productos y pagos del ticket.

--- src/pages/TicketDetail.js
+++ src/pages/TicketDetail.js
@@ -1,197 +1,193 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from "react-router-dom";
-import { Layout, Menu, Dropdown, Row, Col } from 'antd';
+import React from 'react';
+import { Layout, Menu, Dropdown, Row, Col, Card, Avatar } from 'antd';
 import 'antd/dist/antd.css';
 import { useHistory } from "react-router-dom";
-import background2 from '../assets/imgs/background2.png';
 import account from '../assets/imgs/account.png'
 import { useSelector } from 'react-redux'
 import Api from '../api/Api';
 import { DownOutlined } from "@ant-design/icons";
-import { Avatar } from 'antd';
-import  TableProducts  from '../components/TableProducts';
+import StaticTable from '../components/StaticTable';
+import logo from '../assets/imgs/logo.png';
+import '../styles/ticketDetail.scss'
+
 const { Header, Footer, Content } = Layout;
 
 
 const TicketDetail = () => {
     const history = useHistory();
-    const location = useLocation();
-    const user = useSelector((user) => {return user.users.payload})
-    const ticket = useSelector((ticket) => {return ticket.ticket.payload})
+
+    const user = JSON.parse(localStorage.getItem('user'))
+    const tokenInfo = JSON.parse(localStorage.getItem('token'))
+    const userTicket = JSON.parse(localStorage.getItem('userTicket'))
+    const ticket = JSON.parse(localStorage.getItem('ticket'))
     
-    const goToUpdateAccountPage = ()=>{
+    const purchasedProductsColumns = [
+        {
+            title: 'Descripcion',
+            dataIndex: 'description',
+            key: 'description',
+        },
+        {
+            title: 'Precio',
+            dataIndex: 'price',
+            key: 'price',
+        },
+        {
+            title: 'Cantidad',
+            dataIndex: 'quantity',
+            key: 'quantity',
+        },
+        {
+            title: 'Validez',
+            key: 'validity',
+            dataIndex: 'validity'
+        },
+    ];
+
+    const paymentMethodsColumns = [
+        {
+            title: 'Método',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Monto',
+            dataIndex: 'amount',
+            key: 'amount',
+        }
+    ];
+
+    const mapPurchasedProductsData = (dataSource) => {
+        console.log(dataSource.purchased_products)
+        let a = [];
+        a = dataSource.map((item, index) => {
+            let date = new Date(item.product.valid_date_from);
+            return {
+                key: '' + index, description: item.product.description, price: item.product.price,
+                quantity: item.quantity, validity: date.getDay() + '/' + date.getMonth() + '/' + date.getYear()
+                    + ' ' + date.getHours() + ':' + date.getMinutes()
+            }
+        })
+        return a;
+    }
+
+    const mapPaymentMethodsData = (dataSource) => {
+        let result = [];
+        result = dataSource.map((item, index) => {
+            return {
+                key: '' + index, name: item.payment_method.name, amount: item.amount_paid
+            }
+        })
+        return result;
+    }
+
+    const goToUpdateAccountPage = () => {
         history.push("/ehome/account")
+    }
+
+    const goToFirstPage = () => {
+        if(user && tokenInfo)
+            if(user.role.name == "empleado")
+                history.push("/ehome")
+            else
+                history.push("/ohome")
+        else{
+            history.push("")
+        }
+            
     }
 
     const menu = (
         <div>
-          
-          <Menu style={{marginTop:8}}>
-          {user.name + ' '+user.surname}  
-            <Menu.Item key="0" onClick={e=> goToUpdateAccountPage()}>
-              Actualizar datos
+
+            <Menu style={{ marginTop: 8 }}>
+                {user.name + ' ' + user.surname}
+                <Menu.Item key="0" onClick={e => goToUpdateAccountPage()}>
+                    Actualizar datos
             </Menu.Item>
-            <Menu.Item key="1">
-              Cerrar sesión
+                <Menu.Item key="1">
+                    Cerrar sesión
             </Menu.Item>
-          </Menu>
+            </Menu>
         </div>
-      );
+    );
     return (
         <Layout className="layout">
-        <Header style={{background:"#6200EE"}}>
-          <div className="logo" onClick={()=>{ history.push(""); }}></div>
-    
-          <Menu mode="horizontal" defaultSelectedKeys={['2']} style={{float:"right", background:"#6200EE", color:"white"}}>
-            <Menu.Item key="1">Ayuda</Menu.Item>
-            <Menu.Item key="2">
-              <Dropdown overlay={menu} trigger={["click"]}>
-                <a className="ant-dropdown-link" >
-                <Avatar style={{ backgroundImage: `url(${account})`}} />
-                <DownOutlined />
-                </a>
-              </Dropdown>
-            </Menu.Item>
-          </Menu>
-        </Header>
-        <Content>
-          <div className="site-layout-content" style={{ backgroundImage: `url(${background2})`}}>
-            <h1 style={{ textAlign: 'center', color: 'white' }}>
-                Ticket válido
-            </h1>
-            <br>
-            </br>
-                <div className="ticketDetail">
-                    <h2>
-                        CSaleApp
-                    </h2>
-                    <h3>
-                        Recibo
-                    </h3>
-                    <h3>
-                        Codigo de compra: <br></br>{ticket._id}
-                    </h3>
-                    <p>
-                        Fecha: {ticket.date_of_purchase}
-                        <br></br>
-                        Productos:
-                        <br></br>
-                        <TableProducts 
-                            data={ticket.purchased_products}
-                        ></TableProducts>
-                    </p>
-                </div>
-            {/*
-
-                                    <div *ngFor="let purchaseProduct of ticket.purchased_products">
-                                        <ion-row>
-                                            <ion-col class="cell-class">
-                                                {{purchaseProduct.product.description}}
-                                            </ion-col>
-                                            <ion-col class="cell-class">
-                                                {{purchaseProduct.product.price}}
-                                            </ion-col>
-                                            <ion-col class="cell-class">
-                                                {{purchaseProduct.quantity}}
-                                            </ion-col>
-                                            <ion-col class="cell-class">
-                                                {{purchaseProduct.product.valid_date_from}}-{{purchaseProduct.product.valid_date_to}}
-                                            </ion-col>
-                                        </ion-row>
-                                    </div>
-                                </ion-grid>
-                            </ion-item>
-                        </ion-list>
-                    </p>
-                </div>
-
-            <div id="Html2Pdf">
-        <ion-card>
-            <ion-item no-lines>
-                
-            </ion-item>
-
-            <ion-card-content>
-                <ion-list>
-                    <ion-item no-lines>
-                        
-                    </ion-item>
-
-                    <ion-item>
-                        Productos:
-                        <ion-list>
-                            <ion-item text-wrap>
-                                <ion-grid>
-                                    <ion-row>
-                                        <ion-col class="cell-class">
-                                            DescripciÃ³n
-                                        </ion-col>
-                                        <ion-col class="cell-class">
-                                            Precio
-                                        </ion-col>
-                                        <ion-col class="cell-class">
-                                            Cantidad
-                                        </ion-col>
-                                        <ion-col class="cell-class">
-                                            Validez
-                                        </ion-col>
-                                    </ion-row>
-                                    <div *ngFor="let purchaseProduct of ticket.purchased_products">
-                                        <ion-row>
-                                            <ion-col class="cell-class">
-                                                {{purchaseProduct.product.description}}
-                                            </ion-col>
-                                            <ion-col class="cell-class">
-                                                {{purchaseProduct.product.price}}
-                                            </ion-col>
-                                            <ion-col class="cell-class">
-                                                {{purchaseProduct.quantity}}
-                                            </ion-col>
-                                            <ion-col class="cell-class">
-                                                {{purchaseProduct.product.valid_date_from}}-{{purchaseProduct.product.valid_date_to}}
-                                            </ion-col>
-                                        </ion-row>
-                                    </div>
-                                </ion-grid>
-                            </ion-item>
-                        </ion-list>
-                    </ion-item>
-
-                    <ion-item no lines>
-                        Total: ${{ticket.total}}
-                    </ion-item>
-                    <ion-item no lines>
-                        Pagaste con:
-                        <ion-grid *ngFor="let paymentMethod of ticket.payment_methods">
-                            <ion-row>
-                                - {{toCapitalize(paymentMethod.paymentMethod.name)}}
-                            </ion-row>
-                            <div *ngIf="paymentMethod.paymentMethod.name == tarjeta">
-                                <ion-row>
-                                    NÃºmero: paymentMethod.creditCard
-                                </ion-row>
-                            </div>
-                            <ion-row>
-                                Abonaste: ${{paymentMethod.amountPaid}}
-                            </ion-row>
-                        </ion-grid>
-                    </ion-item>
-                    <div>
-                        Nota: cuando lleguÃ©s a la entrada, mostrale al boletero/a o barman/barwoman este ticket, asi aprovechÃ¡s tu compra!.
-                    </div>
-                </ion-list>
-            </ion-card-content>
-        </ion-card>
-    </div>
-
- */}
+        <Header className="headerStyle">
+          <div className="logo" onClick={()=>{goToFirstPage()}}>
+            <img src={logo} width="50" height="50" />
           </div>
-          
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>CSaleAdminWeb (c) 2020</Footer>
-      </Layout>
-      );
-    
+
+                <Menu mode="horizontal" defaultSelectedKeys={['2']} style={{ float: "right", background: "#6200EE", color: "white" }}>
+                    <Menu.Item key="1">Ayuda</Menu.Item>
+                    <Menu.Item key="2">
+                        <Dropdown overlay={menu} trigger={["click"]}>
+                            <a className="ant-dropdown-link" >
+                                <Avatar style={{ backgroundImage: `url(${account})` }} />
+                                <DownOutlined />
+                            </a>
+                        </Dropdown>
+                    </Menu.Item>
+                </Menu>
+            </Header>
+            <Content>
+                <div className="site-layout-content" style={{ background: "#382456", color: "white" }}>
+                    <h1 style={{ color: "white", textAlign: 'center' }}>
+                        Validar ticket
+                    </h1>
+                    <Row>
+                        <Col lg={{ span: 12, push: 3 }}>
+                            <Card className="cardStyle" bordered={false}>
+                                <h2>
+                                    CSaleApp
+                                </h2>
+                                <h3>
+                                    Recibo
+                                </h3>
+                                <h3>
+                                    Codigo de compra: <br></br>{ticket._id}
+                                </h3>
+                                <p>
+                                    Fecha: {ticket.date_of_purchase}
+                                    <br></br>
+                                    Productos:
+                                    <br></br>
+                                    <StaticTable
+                                        data={mapPurchasedProductsData(ticket.purchased_products)}
+                                        columns={purchasedProductsColumns}></StaticTable>
+                                    <br></br>
+                                    Total: ${ticket.total}
+                                        <br></br>
+                                    Pagos realizados:
+                                    <StaticTable
+                                            data={mapPaymentMethodsData(ticket.payment_methods)}
+                                            columns={paymentMethodsColumns}></StaticTable>
+                                    <br></br>
+                                    Nota: cuando llegués a la entrada, mostrale al
+                                    boletero/a o barman/barwoman este ticket, asi aprovechás tu compra!.
+                                </p>
+                            </Card>
+                        </Col>
+                        <Col lg={{ span: 12, push: 3 }}>
+                            <Card className="userCard" bordered={false}>
+                                <h2>
+                                    Detalle del cliente
+                                </h2>
+                                <p>
+                                    Cliente: {userTicket.name + ' ' + userTicket.surname}
+                                    <br></br>
+                                    Email: {userTicket.email}
+                                </p>
+                            </Card>
+
+                        </Col>
+                    </Row>
+                </div>
+            </Content>
+            <Footer className="footer">CSaleAdminWeb (c) 2020</Footer>
+        </Layout>
+    );
+
 
 }
 
