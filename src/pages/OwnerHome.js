94bd72a535dcb6b0d76baaf5da16f265499d86b4
fabcataldo <fabioalbertocataldo@gmail.com>
2020-05-13 17:36:28 -0300

    Para el home de la Vista para dueños y socios, implemento elementos grales de la pág., y agrego acceso a la Gestión de usuarios.

--- src/pages/OwnerHome.js
+++ src/pages/OwnerHome.js
@@ -1,33 +1,80 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Form, Input, Button, Layout, Menu, Dropdown, Row } from 'antd';
 import 'antd/dist/antd.css';
+import { DownOutlined } from "@ant-design/icons";
+import account from '../assets/imgs/account.png'
+import logo from '../assets/imgs/logo.png';
+import { Avatar } from 'antd';
+import background2 from '../assets/imgs/background2.png'
 import { useHistory } from "react-router-dom";
 const { Header, Footer, Content } = Layout;
 
 const OwnerHome = () =>{
   const history = useHistory();
+  
+  const userInfo = JSON.parse(localStorage.getItem('user'))
+  const tokenInfo = JSON.parse(localStorage.getItem('token'))
 
+
+
+  const closeSession = ()=>{
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('ticket');
+    localStorage.removeItem('userTicket');
+    history.push("")
+  }
+
+  const goToUpdateAccountPage = ()=>{
+    history.push("/ohome/account")
+  }
+
+
+  const menu = (
+    <div>
+      
+      <Menu style={{marginTop:8}}>
+      {userInfo.name + ' '+userInfo.surname}  
+        <Menu.Item key="0" onClick={e=> goToUpdateAccountPage()}>
+          Actualizar datos
+        </Menu.Item>
+        <Menu.Item key="1" onClick={e=> closeSession()}>
+          Cerrar sesión
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
   return (
     <Layout className="layout">
-    <Header style={{background:"#6200EE"}}>
-      <div className="logo"  onClick={()=>{ history.push(""); }}/>
-      <Menu mode="horizontal" defaultSelectedKeys={['2']} style={{float:"right", background:"#6200EE", color:"white"}}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
+    <Header className="headerStyle">
+      <div className="logo" onClick={()=>{ history.push(""); }}>
+        <img src={logo} width="50" height="50" />
+      </div>
+
+      <Menu mode="horizontal" style={{float:"right", background:"#6200EE", color:"white"}}>
+        <Menu.Item key="1"  onClick={()=>{ history.push("/ohome/usersAdmin"); }}>Gestionar usuarios</Menu.Item>
+        <Menu.Item key="2">Ayuda</Menu.Item>
+        <Menu.Item key="3">
+          <Dropdown overlay={menu} trigger={["click"]}>
+            <a className="ant-dropdown-link" >
+            <Avatar style={{ backgroundImage: `url(${account})`}} />
+            <DownOutlined />
+            </a>
+          </Dropdown>
+        </Menu.Item>
       </Menu>
     </Header>
-    <Content style={{ padding: '0 50px' }}>
-      <div className="site-layout-content">
-      <h1 style={{ textAlign: 'center' }}>
-        Bienvenido a CSaleAdminWeb! :)
-      </h1>
-      <h3  style={{ textAlign: 'center' }}>
-      ¡Acá vas a poder gestionar tu negocio de manera fácil, rápida y segura!
-      </h3>
+    <Content>
+      <div className="site-layout-content" style={{ backgroundImage: `url(${background2})`}}>
+        <h1 style={{ textAlign: 'center', color: 'white' }}>
+          Bienvenido a CSaleAdminWeb! :)
+        </h1>
+        <h3  style={{ textAlign: 'center', color: 'white' }}>
+        ¡Acá vas a poder gestionar tu negocio de manera fácil, rápida y segura!
+        </h3>
       </div>
     </Content>
-    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    <Footer className="footer">CSaleAdminWeb (c) 2020</Footer>
   </Layout>
   );
 }
