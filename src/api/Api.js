bd1a51dd1296914a207b56c73bcd8b279efbb86e
fabcataldo <fabioalbertocataldo@gmail.com>
2020-05-13 17:33:18 -0300

    Agrego requests para la Gestión de usuarios.

--- src/api/Api.js
+++ src/api/Api.js
@@ -17,6 +17,15 @@ export default class Api{
         return response;
     }
 
+    static getUsers = async(conf)=>{
+        let response;
+        await axios.get(Global.URL_SRV+Global.URL_API+'/users/', conf)
+        .then(res=>{
+            response = res.data;
+        });
+        return response;
+    }
+
     static getTicket = async (ticketId, conf)=>{
         let response;
         await axios.get(Global.URL_SRV+Global.URL_API+'/tickets/'+ticketId, conf)
@@ -38,6 +47,42 @@ export default class Api{
     static updateUser = async(user, conf)=>{
         let response;
         await axios.put(Global.URL_SRV+Global.URL_API+'/users/'+user._id, user, conf)
+        .then(res=>{
+            response = res.data;
+        });
+        return response;
+    }
+
+    static getRoles = async(conf)=>{
+        let response;
+        await axios.get(Global.URL_SRV+Global.URL_API+'/roles/', conf)
+        .then(res=>{
+            response = res.data;
+        });
+        return response;
+    }
+
+    static getRole = async(roleId, conf)=>{
+        let response;
+        await axios.get(Global.URL_SRV+Global.URL_API+'/roles/'+roleId, conf)
+        .then(res=>{
+            response = res.data;
+        });
+        return response;
+    }
+
+    static removeUser = async(userId, conf)=>{
+        let response;
+        await axios.delete(Global.URL_SRV+Global.URL_API+'/users/'+userId, conf)
+        .then(res=>{
+            response = res.data;
+        });
+        return response;
+    }
+
+    static addUser = async(user, conf)=>{
+        let response;
+        await axios.post(Global.URL_SRV+Global.URL_API+'/users/',user, conf)
         .then(res=>{
             response = res.data;
         });
