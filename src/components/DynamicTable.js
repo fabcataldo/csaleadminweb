81d812db0c2f17a26a9a5dc785795848943198ad
fabcataldo <fabioalbertocataldo@gmail.com>
2020-05-25 20:56:47 -0300

    Nueva implementación para el componente de Tabla.

--- src/components/DynamicTable.js
+++ src/components/DynamicTable.js
@@ -5,179 +5,214 @@ import { Table, Input, Button, Popconfirm, Form } from 'antd';
 const EditableContext = React.createContext();
 
 const EditableRow = ({ index, ...props }) => {
-    const [form] = Form.useForm();
-    return (
-        <Form form={form} component={false}>
-            <EditableContext.Provider value={form}>
-                <tr {...props} />
-            </EditableContext.Provider>
-        </Form>
-    );
+  const [form] = Form.useForm();
+  return (
+    <Form form={form} component={false}>
+      <EditableContext.Provider value={form}>
+        <tr {...props} />
+      </EditableContext.Provider>
+    </Form>
+  );
 };
 
 const EditableCell = ({
-    title,
-    editable,
-    children,
-    dataIndex,
-    record,
-    handleSave,
-    ...restProps
+  title,
+  editable,
+  children,
+  dataIndex,
+  record,
+  handleSave,
+  ...restProps
 }) => {
-    const [editing, setEditing] = useState(false);
-    const inputRef = useRef();
-    const form = useContext(EditableContext);
-    useEffect(() => {
-        if (editing) {
-            inputRef.current.focus();
-        }
-    }, [editing]);
-
-    const toggleEdit = () => {
-        setEditing(!editing);
-        form.setFieldsValue({
-            [dataIndex]: record[dataIndex],
-        });
-    };
-
-    const save = async e => {
-        try {
-            const values = await form.validateFields();
-            toggleEdit();
-            handleSave({ ...record, ...values });
-        } catch (errInfo) {
-            console.log('Save failed:', errInfo);
-        }
-    };
-
-    let childNode = children;
-
-    if (editable) {
-        childNode = editing ? (
-            <Form.Item
-                style={{
-                    margin: 0,
-                }}
-                name={dataIndex}
-                rules={[
-                    {
-                        required: true,
-                        message: `${title} is required.`,
-                    },
-                ]}
-            >
-                <Input ref={inputRef} onPressEnter={save} onBlur={save} />
-            </Form.Item>
-        ) : (
-                <div
-                    className="editable-cell-value-wrap"
-                    style={{
-                        paddingRight: 24,
-                    }}
-                    onClick={toggleEdit}
-                >
-                    {children}
-                </div>
-            );
+  const [editing, setEditing] = useState(false);
+  const inputRef = useRef();
+  const form = useContext(EditableContext);
+  useEffect(() => {
+    if (editing) {
+      inputRef.current.focus();
     }
+  }, [editing]);
 
-    return <td {...restProps}>{childNode}</td>;
+  const toggleEdit = () => {
+    setEditing(!editing);
+    form.setFieldsValue({
+      [dataIndex]: record[dataIndex],
+    });
+  };
+
+  const save = async e => {
+    try {
+      const values = await form.validateFields();
+      toggleEdit();
+      handleSave({ ...record, ...values });
+    } catch (errInfo) {
+      console.log('Save failed:', errInfo);
+    }
+  };
+
+  let childNode = children;
+
+  if (editable) {
+    childNode = editing ? (
+      <Form.Item
+        style={{
+          margin: 0,
+        }}
+        name={dataIndex}
+        rules={[
+          {
+            required: true,
+            message: `${title} is required.`,
+          },
+        ]}
+      >
+        <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+      </Form.Item>
+    ) : (
+      <div
+        className="editable-cell-value-wrap"
+        style={{
+          paddingRight: 24,
+        }}
+        onClick={toggleEdit}
+      >
+        {children}
+      </div>
+    );
+  }
+
+  return <td {...restProps}>{childNode}</td>;
 };
 
-
-const DynamicTable = (props) => {
-    const { data, columns } = props;
-    const [count, setCount] = useState(0);
-
-    const handleDelete = key => {
-        const dataSource = [...this.state.dataSource];
-        this.setState({
-            dataSource: dataSource.filter(item => item.key !== key),
-        });
+class DynamicTable extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('this.props.data: '+Array.isArray(this.props.data));
+    this.columns = this.props.columns;
+    this.state = {
+      dataSource: Array.isArray(this.props.data) ? this.props.data : [
+        {
+          key: '0',
+          name: 'Edward King 0',
+          surname: 'London, Park Lane no. 0',
+          email: '32',
+          
+        },
+        {
+          key: '1',
+          name: 'Edward King 1',
+          surname: 'London, Park Lane no. 1',
+          email: '32',
+        },
+      ],
+      count: 0,
     };
-
-    const handleAdd = () => {
-        const newData = {
-            key: count,
-            name: `Usuario `,
-            surname: 'Nuevo usuario',
-            role: {},
-            privileges: {}
-        };
-        useCount(count+1)
-        data.push(newData)
-    };
-
-    const handleSave = row => {
-        const newData = [...this.state.dataSource];
-        const index = newData.findIndex(item => row.key === item.key);
-        const item = newData[index];
-        newData.splice(index, 1, { ...item, ...row });
-        this.setState({
-            dataSource: newData,
-        });
-    };
-
-    let dynamicTableColumns = columns;
-    dynamicTableColumns.push({
+    this.columns.push({
         title: 'Acciones',
         dataIndex: 'operations',
         render: (text, record) =>
             this.state.dataSource.length >= 1 ? (
-                <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
+                <Popconfirm title="Sure to delete?" onConfirm={() => this.handleDelete(record.key)}>
                     <a>Delete</a>
                 </Popconfirm>
             ) : null,
     })
+  }
+
+
+
+mapUsersData = (dataSource) => {
+    let a = [];
+    console.log(dataSource)
+    if(Array.isArray(dataSource)){
+        a = dataSource.map((item, index) => {
+            return {
+                key: '' + index, name: item.name, surname: item.surname, email: item.email  
+            }
+        })    
+        return a;
+    }
+}
+  
+  handleDelete = key => {
+    const dataSource = [...this.state.dataSource];
+    this.setState({
+      dataSource: dataSource.filter(item => item.key !== key),
+    });
+  };
+
+  handleAdd = () => {
+    const { count, dataSource } = this.state;
+    const newData = {
+      key: count,
+      name: `Edward King ${count}`,
+      age: 32,
+      address: `London, Park Lane no. ${count}`,
+    };
+    this.setState({
+      dataSource: [...dataSource, newData],
+      count: count + 1,
+    });
+  };
+
+  handleSave = row => {
+    const newData = [...this.state.dataSource];
+    const index = newData.findIndex(item => row.key === item.key);
+    const item = newData[index];
+    newData.splice(index, 1, { ...item, ...row });
+    this.setState({
+      dataSource: newData,
+    });
+  };
+
+  render() {
+    const { dataSource } = this.state;
 
     const components = {
-        body: {
-            row: EditableRow,
-            cell: EditableCell,
-        },
+      body: {
+        row: EditableRow,
+        cell: EditableCell,
+      },
     };
+    const columns = this.columns.map(col => {
+      if (!col.editable) {
+        return col;
+      }
 
-    const columns2 = dynamicTableColumns.map(col => {
-        if (!col.editable) {
-            return col;
-        }
-
-        return {
-            ...col,
-            onCell: record => ({
-                record,
-                editable: col.editable,
-                dataIndex: col.dataIndex,
-                title: col.title,
-                handleSave: handleSave,
-            }),
-        };
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          editable: col.editable,
+          dataIndex: col.dataIndex,
+          title: col.title,
+          handleSave: this.handleSave,
+        }),
+      };
     });
+    console.log(columns)
 
 
     return (
-        <div>
-            <Button
-                onClick={handleAdd}
-                type="primary"
-                style={{
-                    marginBottom: 16,
-                }}
-            >
-                Add a row
-                </Button>
-            <Table
-                columns={columns2}
-                components={components}
-                bordered
-                dataSource={data}
-                size="small"
-            />
-        </div>
-
-    )
-
-
+      <div>
+        <Button
+          onClick={this.handleAdd}
+          type="primary"
+          style={{
+            marginBottom: 16,
+          }}
+        >
+          Add a row
+        </Button>
+        <Table
+          components={components}
+          rowClassName={() => 'editable-row'}
+          bordered
+          dataSource={dataSource}
+          columns={columns}
+        />
+      </div>
+    );
+  }
 }
-
 export default DynamicTable;
