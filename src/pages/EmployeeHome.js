609c095cfbff87e229391e47cb812b7380452933
fabcataldo <fabioalbertocataldo@gmail.com>
2020-06-30 16:23:14 -0300

    Home del empleado: limpio código.

--- src/pages/EmployeeHome.js
+++ src/pages/EmployeeHome.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import { Form, Input, Layout, Row } from 'antd';
 import { useHistory } from "react-router-dom";
-import { useDispatch } from 'react-redux'
 import background2 from '../assets/imgs/background2.png'
 import AppLogo from '../components/AppLogo';
 import Api from '../api/Api';
-import { setTicket as storeTicket } from '../actions/ticket';
 import { RightMenuHeader } from '../components/RightMenuHeader';
 const { Header, Footer, Content } = Layout;
 
 
 const EmployeeHome = () => {
   const history = useHistory();
-  const dispatch = useDispatch();
-  
-  const user = JSON.parse(localStorage.getItem('user'));
   const token = JSON.parse(localStorage.getItem('token'));
-
+  
   const onFinish = async values => {
     const configRequest = {
       headers: { Authorization: `${token}` }
     }
-    
     const ticket = await Api.getTicket(values.uniqueCode, configRequest);
     const userTicket = await Api.getUserTicket(ticket._id, configRequest);
-    dispatch(storeTicket(ticket))
+
     localStorage.setItem('ticket', JSON.stringify(ticket))
     localStorage.setItem('userTicket', JSON.stringify(userTicket))
     history.push("/ehome/ticket");
@@ -49,6 +43,8 @@ const EmployeeHome = () => {
           <h3  style={{ textAlign: 'center', color: 'white' }}>
             ¡Acá vas a poder validar las compras de tus clientes!
           </h3>
+          <br></br>
+          <br></br>
           <Form
               layout="vertical"
               name="basic"
@@ -60,7 +56,7 @@ const EmployeeHome = () => {
             >
               <Row style={{ justifyContent: "center" }}>
                 <Form.Item
-                  label="Código de ticket"
+                  label="Código unico de compra"
                   name="uniqueCode"
                   className="labelStyle"
                   rules={[
