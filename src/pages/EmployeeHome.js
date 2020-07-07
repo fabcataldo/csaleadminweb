085594a616bb03e9432ab223c75f570455bdbcb4
fabcataldo <fabioalbertocataldo@gmail.com>
2020-07-07 16:45:54 -0300

    Pág. de inicio empleado: limpio y mejoro código.

--- src/pages/EmployeeHome.js
+++ src/pages/EmployeeHome.js
@@ -5,23 +5,29 @@ import background2 from '../assets/imgs/background2.png'
 import AppLogo from '../components/AppLogo';
 import Api from '../api/Api';
 import { RightMenuHeader } from '../components/RightMenuHeader';
+import Notification from '../components/Notification';
 const { Header, Footer, Content } = Layout;
 
 
 const EmployeeHome = () => {
   const history = useHistory();
   const token = JSON.parse(localStorage.getItem('token'));
-  
+
   const onFinish = async values => {
     const configRequest = {
       headers: { Authorization: `${token}` }
     }
     const ticket = await Api.getTicket(values.uniqueCode, configRequest);
-    const userTicket = await Api.getUserTicket(ticket._id, configRequest);
 
-    localStorage.setItem('ticket', JSON.stringify(ticket))
-    localStorage.setItem('userTicket', JSON.stringify(userTicket))
-    history.push("/ehome/ticket");
+    if (ticket) {
+      const userTicket = await Api.getUserTicket(ticket._id, configRequest);
+      localStorage.setItem('ticket', JSON.stringify(ticket))
+      localStorage.setItem('userTicket', JSON.stringify(userTicket))
+      history.push("/ehome/ticket");
+    }
+    else {
+      Notification('info', 'No se encontró el ticket de compra.\n');
+    }
   };
 
   const onFinishFailed = errorInfo => {
@@ -36,39 +42,39 @@ const EmployeeHome = () => {
         </RightMenuHeader>
       </Header>
       <Content>
-        <div className="site-layout-content" style={{ backgroundImage: `url(${background2})`}}>
+        <div className="site-layout-content" style={{ backgroundImage: `url(${background2})` }}>
           <h1 style={{ textAlign: 'center', color: 'white' }}>
             Bienvenido a CSaleAdminWeb! :)
           </h1>
-          <h3  style={{ textAlign: 'center', color: 'white' }}>
+          <h3 style={{ textAlign: 'center', color: 'white' }}>
             ¡Acá vas a poder validar las compras de tus clientes!
           </h3>
           <br></br>
           <br></br>
           <Form
-              layout="vertical"
-              name="basic"
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-            >
-              <Row style={{ justifyContent: "center" }}>
-                <Form.Item
-                  label="Código unico de compra"
-                  name="uniqueCode"
-                  className="labelStyle"
-                  rules={[
-                    {
-                      required: true,
-                      message: 'Código inválido!',
-                    },
-                  ]}
-                >
-                  <Input />
-                </Form.Item>
-              </Row>
+            layout="vertical"
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Row style={{ justifyContent: "center" }}>
+              <Form.Item
+                label="Código unico de compra"
+                name="uniqueCode"
+                className="labelStyle"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Código inválido!',
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Row>
           </Form>
         </div>
       </Content>
