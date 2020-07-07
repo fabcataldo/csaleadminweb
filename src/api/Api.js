f6ae38af590dd54cfa2c2009b386030bc1b206e6
fabcataldo <fabioalbertocataldo@gmail.com>
2020-07-07 16:48:24 -0300

    Componente Api: limpio código, agrego notificaciones.

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Global from '../utils/Global';
+import Notification from '../components/Notification';
 
 export default class Api{
     static login = async (user)=>{
@@ -13,6 +14,8 @@ export default class Api{
             }
         }).then(res=>{
             response = res.data;
+        }).catch(err=>{
+            Notification('error', 'Inicio de sesión fallido\n', 'Error técnico: '+err);
         });
         return response;
     }
@@ -22,6 +25,9 @@ export default class Api{
         await axios.get(Global.URL_SRV+Global.URL_API+'/users', conf)
         .then(res=>{
             response = res.data;
+        })
+        .catch(err=>{
+            Notification('error', 'No se pudo obtener los usuarios\n', 'Error técnico: '+err);
         });
         return response;
     }
@@ -31,6 +37,9 @@ export default class Api{
         await axios.get(Global.URL_SRV+Global.URL_API+'/tickets/?unique_code='+uniqueCode, conf)
         .then(res=>{
             response = res.data[0];
+        })
+        .catch(err=>{
+            Notification('error', 'No se pudo obtener el ticket\n', 'Error técnico: '+err);
         });
         return response;
     }
@@ -40,6 +49,9 @@ export default class Api{
         await axios.get(Global.URL_SRV+Global.URL_API+'/tickets', conf)
         .then(res=>{
             response = res.data;
+        })
+        .catch(err=>{
+            Notification('error', 'No se pudieron obtener los tickets\n', 'Error técnico: '+err);
         });
         return response;
     }
@@ -49,6 +61,9 @@ export default class Api{
         await axios.get(Global.URL_SRV+Global.URL_API+'/tickets/?date_from='+date_from+'&date_to='+date_to, conf)
         .then(res=>{
             response = res.data;
+        })
+        .catch(err=>{
+            Notification('error', 'No se pudo obtener el ticket\n', 'Error técnico: '+err);
         });
         return response;
     }
@@ -59,6 +74,9 @@ export default class Api{
         await axios.get(Global.URL_SRV+Global.URL_API+'/users/ticket/'+ticketId, conf)
         .then(res=>{
             response = res.data;
+        })
+        .catch(err=>{
+            Notification('error', 'No se pudo obtener el usuario del ticket\n', 'Error técnico: '+err);
         });
         return response;
     }
@@ -68,24 +86,10 @@ export default class Api{
         await axios.put(Global.URL_SRV+Global.URL_API+'/users/'+user._id, user, conf)
         .then(res=>{
             response = res.data;
-        });
-        return response;
-    }
-
-    static getRoles = async(conf)=>{
-        let response;
-        await axios.get(Global.URL_SRV+Global.URL_API+'/roles/', conf)
-        .then(res=>{
-            response = res.data;
-        });
-        return response;
-    }
-
-    static getRole = async(roleId, conf)=>{
-        let response;
-        await axios.get(Global.URL_SRV+Global.URL_API+'/roles/'+roleId, conf)
-        .then(res=>{
-            response = res.data;
+            Notification('success', 'Usuario actualizado.\n');
+        })
+        .catch(err=>{
+            Notification('error', 'Actualización fallida.\n', 'Error técnico: '+err);
         });
         return response;
     }
@@ -95,6 +99,21 @@ export default class Api{
         await axios.get(Global.URL_SRV+Global.URL_API+'/roles', conf)
         .then(res=>{
             response = res.data;
+        })
+        .catch(err=>{
+            Notification('error', 'No se pudieron obtener los roles.\n', 'Error técnico: '+err);
+        });
+        return response;
+    }
+
+    static getRole = async(roleId, conf)=>{
+        let response;
+        await axios.get(Global.URL_SRV+Global.URL_API+'/roles/'+roleId, conf)
+        .then(res=>{
+            response = res.data;
+        })
+        .catch(err=>{
+            Notification('error', 'No se pudo obtener el rol.\n', 'Error técnico: '+err);
         });
         return response;
     }
@@ -104,6 +123,10 @@ export default class Api{
         await axios.delete(Global.URL_SRV+Global.URL_API+'/users/'+userId, conf)
         .then(res=>{
             response = res.data;
+            Notification('success', 'Usuario eliminado.\n');
+        })
+        .catch(err=>{
+            Notification('error', 'No se pudo eliminar el usuario.\n', 'Error técnico: '+err);
         });
         return response;
     }
@@ -113,6 +136,10 @@ export default class Api{
         await axios.post(Global.URL_SRV+Global.URL_API+'/users/',user, conf)
         .then(res=>{
             response = res.data;
+            Notification('success', 'Usuario guardado.\n');
+        })
+        .catch(err=>{
+            Notification('error', 'No se pudo guardar el usuario.\n', 'Error técnico: '+err);
         });
         return response;
     }
