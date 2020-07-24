e725405b5624c1794c961e33c1ed0ef74f06ab84
fabcataldo <fabioalbertocataldo@gmail.com>
2020-07-24 15:35:20 -0300

    Subo URL de heroku

--- src/utils/Global.js
+++ src/utils/Global.js
@@ -1,4 +1,5 @@
 export default class Global{
-    static URL_SRV = "http://192.168.1.161:3000";
+    //static URL_SRV = "http://192.168.1.161:3000";
+    static URL_SRV = "https://csalesrv.herokuapp.com";
     static URL_API = "/api";
 }
