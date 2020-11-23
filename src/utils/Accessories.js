b72e7857e142a5310f5a3b12efc75abe9e4286dd
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2020-11-23 12:50:33 -0300

    Arrelgo fechas.

--- src/utils/Accessories.js
+++ src/utils/Accessories.js
@@ -3,7 +3,10 @@ class Accessories{
         var tmp = new Date(date)
         return tmp.getDate()+'/'+(tmp.getMonth().toString().length < 2 ? '0' 
             + tmp.getMonth() : (tmp.getMonth() + 1) ) +
-            '/'+tmp.getFullYear()+' '+tmp.getHours()+':'+tmp.getMinutes()+':'+tmp.getSeconds();
+            '/'+tmp.getFullYear()+' '+tmp.getHours()+':'+
+            (tmp.getMinutes().toString().length < 2 ? '0' 
+            + tmp.getMinutes() : (tmp.getMinutes() + 1) ) 
+                +':'+tmp.getSeconds();
     }
 }
 
