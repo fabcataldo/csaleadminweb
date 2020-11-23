ed7bb56259d30c996d4a0c093109d37be0518a32
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2020-11-23 12:29:31 -0300

    Arreglo bug en el que no se mostraban bien las fechas del detalle de un ticket, consultado por un empleado.

--- src/utils/Accessories.js
+++ src/utils/Accessories.js
@@ -1,7 +1,9 @@
 class Accessories{
     static formatDate(date){
         var tmp = new Date(date)
-        return tmp.getDay()+'/'+(tmp.getMonth()+1)+'/'+tmp.getFullYear()+' '+tmp.getHours()+':'+tmp.getMinutes()+':'+tmp.getSeconds();
+        return tmp.getDate()+'/'+(newDateOfPurchase.getMonth().toString().length < 2 ? '0' 
+            + newDateOfPurchase.getMonth() : (newDateOfPurchase.getMonth() + 1) ) +
+            '/'+tmp.getFullYear()+' '+tmp.getHours()+':'+tmp.getMinutes()+':'+tmp.getSeconds();
     }
 }
 
